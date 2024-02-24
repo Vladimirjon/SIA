eb6be5e69a85bf6c0f5d54b41383bdd378223e30
Jona4325 <133798156+Jona4325@users.noreply.github.com>
2024-02-24 07:38:14 -0500

    ArduinoControl

--- ArduinoControl/ArduinoControl.java
+++ ArduinoControl/ArduinoControl.java
@@ -1,10 +1,12 @@
 package ArduinoControl;
 
 import com.fazecast.jSerialComm.*;
+
 import java.io.OutputStream;
 import java.util.Scanner;
 
 public class ArduinoControl {
+
     private static final String PORT_NAME = "COM3";
     private static final int BAUD_RATE = 9600;
     private static SerialPort serialPort;
@@ -19,6 +21,7 @@ public class ArduinoControl {
             outputStream = serialPort.getOutputStream();
 
             Scanner scanner = new Scanner(System.in);
+
             String userCommand;
 
             do {
@@ -27,16 +30,13 @@ public class ArduinoControl {
 
                 if (serialPort.isOpen() && outputStream != null) {
                     sendCommand(outputStream, userCommand);
-                    outputStream.flush(); 
                     Thread.sleep(2000);
                 }
             } while (!userCommand.equals("0") && !userCommand.equals("1"));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (serialPort != null) {
-                serialPort.closePort();
-            }
+            serialPort.closePort();
         }
     }
 
