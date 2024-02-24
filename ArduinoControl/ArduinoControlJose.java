a4ef51c739d6759a065ecedc5e48ad5e13565a44
Vladimirjon <johannpasquel@gmail.com>
2024-02-24 17:59:59 -0500

    intentos

--- ArduinoControl/ArduinoControlJose.java
+++ ArduinoControl/ArduinoControlJose.java
@@ -2,14 +2,16 @@ package ArduinoControl;
 
 import com.fazecast.jSerialComm.*;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Scanner;
 
-public class ArduinoControl {
+public class ArduinoControlJose {
 
     private static final String PORT_NAME = "COM3";
     private static final int BAUD_RATE = 9600;
     private static SerialPort serialPort;
+    private static  InputStream inputStream;
     private static OutputStream outputStream;
 
     public static void main(String[] args) {
@@ -18,9 +20,31 @@ public class ArduinoControl {
         try {
             serialPort.setBaudRate(BAUD_RATE);
             serialPort.openPort();
+            inputStream = serialPort.getInputStream();
             outputStream = serialPort.getOutputStream();
 
             Scanner scanner = new Scanner(System.in);
+            
+            
+            Thread receivingThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        while (inputStream.available() == 0) {
+                            Thread.sleep(100);
+                        }
+
+                        // Leer los datos disponibles del Arduino
+                        byte[] buffer = new byte[inputStream.available()];
+                        inputStream.read(buffer);
+
+                        String humidityData = new String(buffer);
+                        System.out.println("Humedad del suelo: " + humidityData.trim() + "%");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            receivingThread.start();
 
             String userCommand;
 
