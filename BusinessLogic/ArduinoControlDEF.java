51bc29a4c7df2ab2bb5db1fbc331f08ae8223896
David Esteban Morales Estrella <davidestebanmoralese@gmail.com>
2024-02-25 10:55:31 -0500

    riego automatico

--- BusinessLogic/ArduinoControlDEF.java
+++ BusinessLogic/ArduinoControlDEF.java
@@ -1,42 +1,35 @@
 package BusinessLogic;
 
 import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ArduinoControlDEF {
 
-    public SerialPort conectionArduino(String puerto){
+    public SerialPort conectionArduino(String puerto) {
         SerialPort port = SerialPort.getCommPort(puerto);
         port.setBaudRate(9600);
         if (!port.openPort()) {
             System.out.println("No se pudo abrir el puerto COM3.");
             return null;
-        }else{
+        } else {
             System.out.println("Conexion exitosa");
             return port;
         }
     }
 
-    public void  readArduino (SerialPort port){
-        int value;
-        Scanner sc = new Scanner(port.getInputStream());
-        try {
-            while (true) {
-                if(sc.hasNextLine()){
-                    String line = sc.nextLine();
-                    value = Integer.parseInt(line);
-                    System.out.println(value); 
-                }
+    public void sendData(int input, SerialPort port) {
+        if (input == 0 || input == 1) {
+            System.out.println("Enviando data: " + input);
+            try {
+                port.getOutputStream().write(Integer.toString(input).getBytes());
                 Thread.sleep(1000);
+            }catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Puerto Cerrado.");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("No se puedo leer");;
-        } finally {
-            sc.close();
-            port.closePort();
-            System.out.println("Puerto Cerrado.");
         }
-
     }
 }
