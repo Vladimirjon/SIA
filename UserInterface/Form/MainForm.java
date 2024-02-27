1fad8143a31b6bcdfba54b3cacd3af1366919d45
DavidME1604 <davidestebanmoralese@gmail.com>
2024-02-27 10:34:43 -0500

    button regar

--- UserInterface/Form/MainForm.java
+++ UserInterface/Form/MainForm.java
@@ -6,6 +6,10 @@ import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.fazecast.jSerialComm.SerialPort;
+
+import BusinessLogic.ArduinoControlDEF;
+import BusinessLogic.ControlRiego;
 import UserInterface.IAStyle;
 
 public class MainForm extends JFrame{
@@ -16,11 +20,19 @@ public class MainForm extends JFrame{
         customizeComponent(tilteApp);
         pnlMenu.btnHome.addActionListener(      e -> setPanel(new MainPanel())); 
         // pnlMenu.btnLogin.addActionListener(     e -> setPanel(new LoginPanel())); 
-        pnlMenu.btnBase.addActionListener(      e -> setPanel(new BasePanel()));  
+        pnlMenu.btnBase.addActionListener(      e -> regadoAutomatico());  
         pnlMenu.btnRegar.addActionListener(      e -> setPanel(new BasePanel()));  
         // pnlMenu.btnLocalidad.addActionListener( e -> setPanel(new MainPanel())); 
         //agregar
         // pnlMenu.btnTest.addActionListener( e -> { IAStyle.showMsgError("mensaje de error");}); 
+    }
+
+    private void regadoAutomatico() {
+        ArduinoControlDEF controlDef = new ArduinoControlDEF();
+        ControlRiego reg = new ControlRiego();
+        SerialPort port;
+        port = controlDef.conectionArduino("COM3");
+        reg.regarManual(port);
     }
 
     private void setPanel(JPanel formularioPanel) {
